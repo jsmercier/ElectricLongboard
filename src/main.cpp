193dbc17ee0732825846847f98dbba9fc6106a2c
jsmercier <js.mercier.a@gmail.com>
2020-08-17 15:45:52 -0400

    change pattern with sterring control

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,159 @@
 #include <Arduino.h>
- 
+#include <EnableInterrupt.h>
+#include <FastLED.h>
+
+
+//definitions RC
+#define SERIAL_PORT_SPEED 9600
+#define RC_CH1_INPUT  5
 int ch1; // Here's where we'll keep our channel values
-int ch2;
-int ch3;
+
+
+//definitions LED
+FASTLED_USING_NAMESPACE
+#if defined(FASTLED_VERSION) && (FASTLED_VERSION < 3001000)
+#warning "Requires FastLED 3.1 or later; check github for latest code."
+#endif
+
+#define DATA_PIN    2
+//#define CLK_PIN   4
+#define LED_TYPE    NEOPIXEL
+#define COLOR_ORDER GRB
+#define NUM_LEDS    40
+CRGB leds[NUM_LEDS];
+
+#define BRIGHTNESS          250
+#define FRAMES_PER_SECOND  120
+
+unsigned long startMillis;  //some global variables available anywhere in the program
+unsigned long currentMillis;
+const unsigned long period = 1000/FRAMES_PER_SECOND; 
+
+
+uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
+uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+
+#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
+
+void finish(){
+  FastLED.clear();  // clear all pixel data
+}
+
+void rainbow() 
+{
+  // FastLED's built-in rainbow generator
+  fill_rainbow( leds, NUM_LEDS, gHue, 7);
+}
+
+void addGlitter( fract8 chanceOfGlitter) 
+{
+  if( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS) ] += CRGB::White;
+  }
+}
+
+void rainbowWithGlitter() 
+{
+  // built-in FastLED rainbow, plus some random sparkly glitter
+  rainbow();
+  addGlitter(80);
+}
+
+void confetti() 
+{
+  // random colored speckles that blink in and fade smoothly
+  fadeToBlackBy( leds, NUM_LEDS, 10);
+  int pos = random16(NUM_LEDS);
+  leds[pos] += CHSV( gHue + random8(64), 200, 255);
+}
+
+void sinelon()
+{
+  // a colored dot sweeping back and forth, with fading trails
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  int pos = beatsin16( 13, 0, NUM_LEDS-1 );
+  leds[pos] += CHSV( gHue, 255, 192);
+}
+
+void bpm()
+{
+  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
+  uint8_t BeatsPerMinute = 62;
+  CRGBPalette16 palette = PartyColors_p;
+  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
+  for( int i = 0; i < NUM_LEDS; i++) { //9948
+    leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
+  }
+}
+
+void juggle() {
+  // eight colored dots, weaving in and out of sync with each other
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  byte dothue = 0;
+  for( int i = 0; i < 8; i++) {
+    leds[beatsin16( i+7, 0, NUM_LEDS-1 )] |= CHSV(dothue, 200, 255);
+    dothue += 32;
+  }
+}
+
+// List of patterns to cycle through.  Each is defined as a separate function below.
+typedef void (*SimplePatternList[])();
+SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm, finish };
+
+void nextPattern()
+{
+  // add one to the current pattern number, and wrap around at the end
+  gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
+}
 
 void setup() {
+  Serial.begin(SERIAL_PORT_SPEED);
 
-  pinMode(5, INPUT); // Set our input pins as such
+  pinMode(RC_CH1_INPUT, INPUT);
 
-  Serial.begin(9600); // Pour a bowl of Serial
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
 
+  startMillis = millis();  //initial start time
+   
 }
 
 void loop() {
 
+currentMillis = millis();
+
   ch1 = pulseIn(5, HIGH, 25000); // Read the pulse width of 
+    if (ch1==0){
+      
+      }
+      else{
+        if(ch1<1000 or ch1>2000){
+            Serial.print("CH1:"); Serial.println(ch1);
+            nextPattern();
+            delay(300);
+        }
+      }
+  
+  //Mettre code pour strip LED
 
-  Serial.print("Channel 1: "); // Print the value of 
-  Serial.println(ch1);        // each channel
+  
 
+// Call the current pattern function once, updating the 'leds' array
+  gPatterns[gCurrentPatternNumber]();
 
-  delay(100); // I put this here just to make the terminal 
-              // window happier
+  // send the 'leds' array out to the actual LED strip
+  FastLED.show();  
+  // insert a delay to keep the framerate modest
+  //FastLED.delay(1000/FRAMES_PER_SECOND); 
+
+  if (currentMillis - startMillis >= period)  //test whether the period has elapsed
+  {
+    gHue++;
+    startMillis = currentMillis;  //IMPORTANT to save the start time of the current LED state.
+  }
+
+  // do some periodic updates
+  
+  //EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
+  //EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
 }
